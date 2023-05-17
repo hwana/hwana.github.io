2935ddf559eed3f6421d1bed98958a989fae6819
hwana <dsa1461@gmail.com>
2023-05-18 00:32:54 +0900

    image 확장자 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -15,7 +15,7 @@ module.exports = {
     bio: {
       role: `백엔드 개발자`,
       description: ['소통을 즐기는', '능동적으로 일하는'],
-      thumbnail: 'hwana.png', // Path to the image in the 'asset' folder
+      thumbnail: 'hwana.jpg', // Path to the image in the 'asset' folder
     },
     social: {
       github: ``, // `https://github.com/zoomKoding`,
