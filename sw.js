2299895f8af20be18ac5dae5dfb7fe2c73aa29b9
hwana <dsa1461@gmail.com>
2023-10-26 20:14:03 +0900

    Updates

--- sw.js
+++ sw.js
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c4a548eba96b864856208d0a25d25b35"
+    "revision": "606ff3a53fed1845e69d07eacc9cbff2"
   },
   {
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
